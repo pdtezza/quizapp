251d34e7021ecc48babe4832aa50af395b7f5365
Pedro tezza <pdtezza@gmail.com>
2024-06-14 17:12:44 +0000

    coloquei o ponto no classBotao pois tinha esquecido

--- scrip.js
+++ scrip.js
@@ -17,7 +17,7 @@ botoesAssunto.forEach(botao =>{
 
 function selecionarAssunto(evento){
     const classeBotao = evento.target.className
-    const assunto = document.querySelector(`${classeBotao} span`).innerText
+    const assunto = document.querySelector(`.${classeBotao} span`).innerText
     localStorage.setItem('assunto',assunto)
     window.location.href='./pages/quiz/quiz.html'
 }
