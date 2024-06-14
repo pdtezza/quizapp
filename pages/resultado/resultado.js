e9eb315692b1b6f911485e04de2e356812ab6361
Pedro tezza <pdtezza@gmail.com>
2024-06-14 16:52:59 +0000

    adicionada a funcao de inserir o resultado na tela

--- pages/resultado/resultado.js
+++ pages/resultado/resultado.js
@@ -2,6 +2,7 @@ import { trocarTema,verificarTema } from "../../helpers/tema-helper.js"
 
 const botaoTema = document.querySelector(".tema button")
 const body=document.querySelector("body")
+const assunto = localStorage.getItem('assunto')
 
 botaoTema.addEventListener("click", ()=>{
     trocarTema( body, botaoTema )
@@ -9,3 +10,30 @@ botaoTema.addEventListener("click", ()=>{
 
 
 verificarTema( body ,botaoTema)
+
+function alterarAssunto(){
+    const divIcone = document.querySelector('.assunto_icone')
+    const iconeImg = document.querySelector('.assunto img')
+    const assuntoTitulo = document.querySelector('.assunto h1')
+
+    divIcone.classList.add(assunto.toLocaleLowerCase())
+    iconeImg.setAttribute('src',`../../assets/images/icon-${assunto.toLowerCase()}.svg`)
+    iconeImg.setAttribute('alt',`icone de ${assunto}`)
+    assuntoTitulo.innerText = assunto
+    
+}
+alterarAssunto()
+function inserirResultado(){
+    const sectionPontuacao=document.querySelector('.pontuacao')
+    const divAssunto =document.querySelector('.assunto')
+    const pontos =localStorage.getItem("pontos")
+
+    sectionPontuacao.innerHTML=`
+        ${divAssunto.outerHTML}
+    
+        <strong>${pontos}</strong>
+    
+        <p>${pontos} de 10</p>
+    `
+}
+inserirResultado()
